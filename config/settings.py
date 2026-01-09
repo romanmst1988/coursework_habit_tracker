7b4f1c86ddf6a5b534563010e8097b2bde0c7ec2
Роман Мстиславский <mstislavskijr@gmail.com>
2026-01-09 16:52:13 +0300

    create model users, hadits

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,8 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
+AUTH_USER_MODEL = 'users.User'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    telegram_chat_id = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+    )
+
+
