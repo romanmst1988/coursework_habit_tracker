81f3aaa45fbb1f78e5b813fb9367fbf95c4fbffc
Роман Мстиславский <mstislavskijr@gmail.com>
2026-01-13 15:43:05 +0300

    fix SIMPLE_JWT

--- config/settings.py
+++ config/settings.py
@@ -151,8 +151,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
